98fb15850f427692f16d65d036f871a4b1dfed5f
deboranobre <deborarbnobre@gmail.com>
2018-05-12 20:49:44 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Products></Products  >
+  </div>
+</template>
+
+<script>
+import Products from './components/Products.vue'
+
+export default {
+  components: {
+    Products
+  }
+}
+</script>
+
+<style>
+</style>
