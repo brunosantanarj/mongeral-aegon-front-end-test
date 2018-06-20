7f73d724baa94ad7bf90cf73d3efa2b15b0a823d
rafaelmagalhaess <rafaremagalhaes@gmail.com>
2018-06-19 21:10:32 -0300

    creating folder structure

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import '../styles/App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        My Store
+      </div>
+    );
+  }
+}
+
+export default App;
